e30f689cd75c5c58e72a3fa1e353d57940b6c102
Christina Forbes <72985770+christinaforbes@users.noreply.github.com>
2024-09-20 17:08:40 -0500

    Created GUI

---
+++ sprint1_0.product/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace sprint1_0.product
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
+
+}
