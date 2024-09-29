7db1d2279c8c8aa76e9f7a8aa50c5219555b0c5e
Christina Forbes <72985770+christinaforbes@users.noreply.github.com>
2024-09-29 12:43:43 -0500

    Modified game settings

--- sprint2_0.product/MainWindow.xaml.cs
+++ sprint2_0.product/MainWindow.xaml.cs
@@ -16,9 +16,102 @@ namespace SoSGame
   /// </summary>
   public partial class MainWindow : Window
   {
+    private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
     public MainWindow()
     {
       InitializeComponent();
+    }
+
+    private void CreateNewGameBoard() {
+      GameBoard.Children.Clear();
+      GameBoard.RowDefinitions.Clear();
+      GameBoard.ColumnDefinitions.Clear();
+
+      int boardSize = Convert.ToInt32(BoardSize.Text);
+      int gridSquareSize = 100 / boardSize;
+
+      for (int i = 0; i < boardSize; i++) {
+        RowDefinition rowDefinition = new RowDefinition();
+        rowDefinition.Height = new GridLength(gridSquareSize, GridUnitType.Star);
+        GameBoard.RowDefinitions.Add(rowDefinition);
+
+        ColumnDefinition columnDefinition = new ColumnDefinition();
+        columnDefinition.Width = new GridLength(gridSquareSize, GridUnitType.Star);
+        GameBoard.ColumnDefinitions.Add(columnDefinition);
+      }
+
+      for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+          Button gameBoardSquare = new Button();
+          gameBoardSquare.Name = $"Square{i}{j}";
+          gameBoardSquare.SetValue(Grid.RowProperty, i);
+          gameBoardSquare.SetValue(Grid.ColumnProperty, j);
+          gameBoardSquare.FontSize = 24;
+          gameBoardSquare.FontWeight = FontWeights.Bold;
+          gameBoardSquare.Background = Brushes.WhiteSmoke;
+          gameBoardSquare.Click += BoardSquare_Click;
+          GameBoard.Children.Add(gameBoardSquare);
+        }
+      }
+
+      GameState.CreateNewGameBoardContents(boardSize);
+    }
+
+    private void SetGameMode() {
+      if (SimpleGame.IsChecked == true) {
+        GameState.UpdateGameMode('S');
+      }
+      else if (GeneralGame.IsChecked == true) {
+        GameState.UpdateGameMode('G');
+      }
+
+      string gameMode = (GameState.GameMode == 'S') ? "simple" : "general";
+      string message = $"You're now playing a {gameMode} game!";
+      MessageBoxResult gameModeMessageBox = MessageBox.Show(message, "", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+
+    private void StartNewGame() {
+      CreateNewGameBoard();
+      SetGameMode();
+
+      CurrentPlayer.Text = "Blue";
+      CurrentPlayer.Foreground = Brushes.Blue;
+      BluePlayerS.IsChecked = true;
+      RedPlayerS.IsChecked = true;
+
+      GameState.ResetGameState();
+    }
+
+    private void GameMode_Checked(object sender, RoutedEventArgs e) {
+      if (GameGrid.IsLoaded && !GameState.GameInProgress) {
+        SetGameMode();
+      }
+    }
+
+    private async void BoardSize_TextChanged(object sender, TextChangedEventArgs e) {
+      _cancellationTokenSource.Cancel();
+      _cancellationTokenSource = new CancellationTokenSource();
+      CancellationToken cancellationToken = _cancellationTokenSource.Token;
+
+      try {
+        await Task.Delay(500, cancellationToken);
+
+        if (!cancellationToken.IsCancellationRequested) {
+          string boardSizeTextBoxVal = BoardSize.Text;
+
+          if (!GameLogic.IsBoardSizeValid(boardSizeTextBoxVal) && boardSizeTextBoxVal != "") {
+            string errorMessage = "Please enter a valid integer between 3 and 10, inclusive";
+            MessageBoxResult errorMessageBox = MessageBox.Show(errorMessage, "Invalid Board Size", MessageBoxButton.OK, MessageBoxImage.Error);
+          }
+          else if (GameLogic.IsBoardSizeValid(boardSizeTextBoxVal) && !GameState.GameInProgress) {
+            CreateNewGameBoard();
+          }
+        }
+      }
+      catch (TaskCanceledException) {
+        return;
+      }
     }
 
     private void BluePlayerLetter_Checked(object sender, RoutedEventArgs e) {
@@ -27,8 +120,6 @@ namespace SoSGame
       } else if (BluePlayerO.IsChecked == true) {
         GameState.UpdateBluePlayerLetter('O');
       }
-
-      return;
     }
 
     private void RedPlayerLetter_Checked(object sender, RoutedEventArgs e) {
@@ -39,7 +130,7 @@ namespace SoSGame
       }
     }
 
-    private void BoardSquareButton_Click(object sender, RoutedEventArgs e)
+    private void BoardSquare_Click(object sender, RoutedEventArgs e)
     {
       Button boardSquare = (Button)sender;
 
@@ -75,79 +166,7 @@ namespace SoSGame
       }
     }
 
-    private void BoardSize_TextChanged(object sender, TextChangedEventArgs e) {
-      string boardSizeTextBoxVal = BoardSize.Text;
-
-      if (!GameLogic.IsBoardSizeValid(boardSizeTextBoxVal) && boardSizeTextBoxVal != "") {
-        string errorMessage = "Please enter a valid integer between 3 and 10, inclusive";
-        MessageBoxResult errorMessageBox = MessageBox.Show(errorMessage, "Invalid Board Size", MessageBoxButton.OK, MessageBoxImage.Error);
-      }
-
-      return;
-    }
-
-    private void CreateNewGameBoard() {
-      // create game board
-      GameBoard.Children.Clear();
-      GameBoard.RowDefinitions.Clear();
-      GameBoard.ColumnDefinitions.Clear();
-
-      int boardSize = Convert.ToInt32(BoardSize.Text);
-      int gridSquareSize = 100 / boardSize;
-
-      for (int i = 0; i < boardSize; i++) {
-        RowDefinition rowDefinition = new RowDefinition();
-        rowDefinition.Height = new GridLength(gridSquareSize, GridUnitType.Star);
-        GameBoard.RowDefinitions.Add(rowDefinition);
-
-        ColumnDefinition columnDefinition = new ColumnDefinition();
-        columnDefinition.Width = new GridLength(gridSquareSize, GridUnitType.Star);
-        GameBoard.ColumnDefinitions.Add(columnDefinition);
-      }
-
-      for (int i = 0; i < boardSize; i++) {
-        for (int j = 0; j < boardSize; j++) {
-          Button gameBoardSquare = new Button();
-          gameBoardSquare.Name = $"Square{i}{j}";
-          gameBoardSquare.SetValue(Grid.RowProperty, i);
-          gameBoardSquare.SetValue(Grid.ColumnProperty, j);
-          gameBoardSquare.FontSize = 24;
-          gameBoardSquare.FontWeight = FontWeights.Bold;
-          gameBoardSquare.Background = Brushes.WhiteSmoke;
-          gameBoardSquare.Click += BoardSquareButton_Click;
-          GameBoard.Children.Add(gameBoardSquare);
-        }
-      }
-
-      GameState.CreateNewGameBoardContents(boardSize);
-    }
-
-    private void SetGameMode() {
-      if (SimpleGame.IsChecked == true) {
-        GameState.UpdateGameMode('S');
-      }
-      else if (GeneralGame.IsChecked == true) {
-        GameState.UpdateGameMode('G');
-      }
-
-      string gameMode = (GameState.GameMode == 'S') ? "simple" : "general";
-      string message = $"You're now playing a {gameMode} game!";
-      MessageBoxResult gameModeMessageBox = MessageBox.Show(message, "", MessageBoxButton.OK, MessageBoxImage.Information);
-    }
-
-    private void StartNewGame() {
-      CreateNewGameBoard();
-      SetGameMode();
-
-      CurrentPlayer.Text = "Blue";
-      CurrentPlayer.Foreground = Brushes.Blue;
-      BluePlayerS.IsChecked = true;
-      RedPlayerS.IsChecked = true;
-
-      GameState.ResetGameState();
-    }
-
-    private void NewGameButton_Click(object sender, RoutedEventArgs e) {
+    private void NewGame_Click(object sender, RoutedEventArgs e) {
       if (GameState.GameInProgress) {
         string confirmMessage = "Are you sure you want to start a new game?";
         MessageBoxResult confirmMessageBox = MessageBox.Show(confirmMessage, "", MessageBoxButton.YesNo, MessageBoxImage.Warning);
